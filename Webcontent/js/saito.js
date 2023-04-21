7c8d8d145834ead91d928576546aa36c2b88fc26
Watanabeami <ami.watanabe@ks-japan.jp>
2023-04-21 16:28:54 +0900

    はじめてのコミット

---
+++ Webcontent/js/saito.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="author" content="自己紹介サイト">
+<meta name="description" content="">
+<title>自己紹介サイト</title>
+<link href="css/saito.css" media="all" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+<h1>Watanabe Ami</h1>
+</header>
+<div>
+<nav>
+<ul>
+<li><a href="hometown.html">Home town</a></li>
+<li><a href="favorite.html">Favorite</a></li>
+<li><a href="life.html">life</a></li>
+</ul>
+</nav>
+<main>
+<h2>Home town Gifu</h2>
+<section>
+</section>
+</main>
+</div>
+<footer>
+<h3>Contact</h3>
+<p>ami.watanabe@ks-japan.jp</p>
+</footer>
+</body>
+</html>    
